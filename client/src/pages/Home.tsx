346f83fcec71c960504aab2d573e239dffb5710a
zakisudev@gmail.com <zakisu250@gmail.com>
2024-02-15 11:56:07 +0300

    Refactor Home component styling
    * Changed the display method to grid
    * Added more space to the songs table

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,29 +1,37 @@
 import styled from '@emotion/styled';
 import SongsTable from '../components/SongsTable';
 import StatsTable from '../components/StatsTable';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const MainContainer = styled.main`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  width: 95vw;
+  height: 85vh;
+  padding: 0;
+  padding-top: 1em;
+  background-color: lightgray;
+  border-radius: 5px;
+  color: black;
+  overflow-y: auto;
+`;
+const MusicContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  width: 100%;
+  max-height: 100%;
+`;
+const StatContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  float: right;
+  width: 100%;
+`;
 
 const Home = () => {
-  const MainContainer = styled.main`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    width: 100%;
-    height: 100vh;
-    background-color: white;
-    overflow: hidden;
-  `;
-  const MusicContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: lightblue;
-  `;
-  const StatContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: lightgreen;
-  `;
-
   return (
     <MainContainer>
       <MusicContainer>
@@ -32,6 +40,17 @@ const Home = () => {
       <StatContainer>
         <StatsTable />
       </StatContainer>
+      <ToastContainer
+        position="top-center"
+        autoClose={1500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </MainContainer>
   );
 };
