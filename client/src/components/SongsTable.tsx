82cb76f8fc2e1fbfcbae989c54b8b68726f6e487
zakisudev@gmail.com <zakisu250@gmail.com>
2024-02-15 11:50:43 +0300

    Add song table functionality
    * Added functionality to List songs in a table
    * Styled components using Styled system

--- client/src/components/SongsTable.tsx
+++ client/src/components/SongsTable.tsx
@@ -1,12 +1,116 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchSongs } from './../services/api';
 import { setSongs } from '../app/song.slice';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../app/root.reducer';
+import { TbMusicPlus, TbEdit, TbHttpDelete } from 'react-icons/tb';
+import styled from '@emotion/styled';
+import AddSongModal from './AddSongModal';
+import UpdateSongModal from './UpdateSongModal';
+
+export interface Song {
+  _id: string;
+  title: string;
+  artist: string;
+  album: string;
+  genre: string;
+}
+
+const SongHeader = styled.h1`
+  text-align: center;
+  font-size: 2em;
+  margin: 0;
+  width: 100%;
+  text-transform: uppercase;
+  font-weight: 500;
+  color: #1a1a1a;
+  text-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+`;
+const SongsTableContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-height: 100%;
+`;
+const AddButton = styled.button`
+  display: flex;
+  gap: 0.5em;
+  align-items: center;
+  margin-left: auto;
+  margin-bottom: 1em;
+  border-radius: 8px;
+  border: 2px solid #1a1a1a;
+  padding: 0.5em 1em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: #1a1a1a;
+  cursor: pointer;
+  transition: border-color 0.25s;
+`;
+const ButtonContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  gap: 1em;
+`;
+const Table = styled.table`
+  width: 97%;
+  border-collapse: collapse;
+  thead {
+    background-color: #1a1a1a;
+    color: white;
+    th {
+      padding: 0.5em 1em;
+    }
+  }
+  tbody {
+    tr {
+      &:nth-of-type(odd) {
+        background-color: #f2f2f2;
+      }
+      &:hover {
+        background-color: #e6e6e6;
+      }
+      td {
+        padding: 0.5em 1em;
+      }
+    }
+  }
+`;
+const Td = styled.td`
+  display: flex;
+  gap: 0.5em;
+  button {
+    padding: 0.5em;
+    border: none;
+    border-radius: 8px;
+    background-color: #1a1a1a;
+    color: white;
+    cursor: pointer;
+    transition: background-color 0.25s;
+    &:hover {
+      background-color: gray;
+    }
+  }
+`;
 
 const SongsTable = () => {
+  const [addModal, setAddModal] = useState(false);
+  const [updateModal, setUpdateModal] = useState(false);
+  const [editingSong, setEditingSong] = useState({});
+
   const { songs } = useSelector((state: RootState) => state.songs);
   const dispatch = useDispatch();
+
+  const handleSongSelect = (song: Song) => () => {
+    setUpdateModal(true);
+    setEditingSong(song);
+  };
 
   useEffect(() => {
     const fetchSongsFromBackend = async () => {
@@ -18,18 +122,33 @@ const SongsTable = () => {
       }
     };
     fetchSongsFromBackend();
-  }, []);
+  }, [dispatch]);
 
   return (
-    <div>
-      <h1>Songs Table</h1>
-      <table>
+    <SongsTableContainer>
+      {addModal && <AddSongModal onClose={() => setAddModal(false)} />}
+
+      {updateModal && (
+        <UpdateSongModal
+          onClose={() => setUpdateModal(false)}
+          song={editingSong as Song}
+        />
+      )}
+      <SongHeader>Songs:</SongHeader>
+      <ButtonContainer>
+        <AddButton onClick={() => setAddModal(true)}>
+          <TbMusicPlus />
+          Add Song
+        </AddButton>
+      </ButtonContainer>
+      <Table>
         <thead>
           <tr>
             <th>Title</th>
             <th>Artist</th>
             <th>Album</th>
             <th>Genre</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
@@ -39,11 +158,19 @@ const SongsTable = () => {
               <td>{song.artist}</td>
               <td>{song.album}</td>
               <td>{song.genre}</td>
+              <Td>
+                <button onClick={handleSongSelect(song)}>
+                  <TbEdit />
+                </button>
+                <button>
+                  <TbHttpDelete />
+                </button>
+              </Td>
             </tr>
           ))}
         </tbody>
-      </table>
-    </div>
+      </Table>
+    </SongsTableContainer>
   );
 };
 
--- client/src/components/StatsTable.tsx
+++ client/src/components/StatsTable.tsx
@@ -6,25 +6,68 @@ import { RootState } from '../app/root.reducer';
 import StatByGenre from './StatByGenre';
 import StatByArtist from './StatByArtist';
 import StatByAlbum from './StatByAlbum';
+import styled from '@emotion/styled';
 
-interface StatsProps {
-  totalSongs: number;
-  totalArtists: number;
-  totalAlbums: number;
-  totalGenres: number;
-  genreCounts: { _id: string; count: number }[];
-  artistAlbumCounts: {
-    totalSongs: number;
-    artist: string;
-    totalAlbums: number;
-  }[];
-  songStats: { _id: string; totalSongs: number }[];
-}
+const StatHeader = styled.h1`
+  text-align: center;
+  margin: 0;
+  padding: 0;
+  font-size: 2em;
+  width: 100%;
+  text-transform: uppercase;
+  font-weight: 500;
+  color: #1a1a1a;
+  text-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+`;
+const StatContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  gap: 1em;
+  justify-content: start;
+  align-items: center;
+  width: 100%;
+  max-height: 100%;
+  overflow: scroll;
+`;
+const StatsDetailHeader = styled.h2`
+  margin: 0;
+  padding: 0;
+  font-size: 1.5em;
+  font-weight: 600;
+  text-decoration: underline;
+  text-align: left;
+  color: #1a1a1a;
+  margin-bottom: 0em;
+`;
+const GenStatContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5em;
+  width: 300px;
+  margin: 0 auto;
+  padding: 0 2em;
+`;
+const GenStatDetails = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 0 1em;
+  font-size: 1.2em;
+  font-weight: 500;
+`;
+const DetailsDiv = styled.div`
+  margin: 0;
+  padding: 0;
+  font-size: 0.75em;
+  font-weight: 500;
+  text-transform: uppercase;
+`;
 
 const StatsTable = () => {
-  const stats: StatsProps = useSelector(
-    (state: RootState) => state.stats.stats
-  );
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const { stats }: any = useSelector((state: RootState) => state.stats);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -41,46 +84,52 @@ const StatsTable = () => {
       }
     };
     fetchStatsFromBackend();
-  }, []);
+  }, [dispatch]);
 
   return (
-    <div>
-      <h1>Songs Stats:</h1>
-      <div>
-        <h2>General stats:</h2>
-        <div>
-          <p>Total # of Songs</p>
-          <p>{stats?.totalSongs}</p>
-        </div>
-        <div>
-          <p>Total # of Artists</p>
-          <p>{stats?.totalArtists}</p>
-        </div>
-        <div>
-          <p>Total # of Albums</p>
-          <p>{stats?.totalAlbums}</p>
-        </div>
-        <div>
-          <p>Total # of Genres</p>
-          <p>{stats?.totalGenres}</p>
-        </div>
-      </div>
-      <div>
-        <h2>Stats in detail:</h2>
-        <div>
-          <h3>By Genre</h3>
-          <StatByGenre stats={stats?.genreCounts} />
-        </div>
-        <div>
-          <h3>By Artist</h3>
-          <StatByArtist stats={stats?.artistAlbumCounts} />
-        </div>
-        <div>
-          <h3>By Album</h3>
-          <StatByAlbum stats={stats?.songStats} />
-        </div>
-      </div>
-    </div>
+    <StatContainer>
+      <StatHeader>Songs Stats:</StatHeader>
+      <GenStatContainer>
+        <StatsDetailHeader>General stats:</StatsDetailHeader>
+        <GenStatDetails>
+          <DetailsDiv>Total # of Songs</DetailsDiv>
+          <DetailsDiv>{stats?.totalSongs}</DetailsDiv>
+        </GenStatDetails>
+        <GenStatDetails>
+          <DetailsDiv>Total # of Artists</DetailsDiv>
+          <DetailsDiv>{stats?.totalArtists}</DetailsDiv>
+        </GenStatDetails>
+        <GenStatDetails>
+          <DetailsDiv>Total # of Albums</DetailsDiv>
+          <DetailsDiv>{stats?.totalAlbums}</DetailsDiv>
+        </GenStatDetails>
+        <GenStatDetails>
+          <DetailsDiv>Total # of Genres</DetailsDiv>
+          <DetailsDiv>{stats?.totalGenres}</DetailsDiv>
+        </GenStatDetails>
+      </GenStatContainer>
+      <GenStatContainer>
+        <StatsDetailHeader>Stats in detail:</StatsDetailHeader>
+        <GenStatDetails>
+          <DetailsDiv>
+            <StatByGenre genreStat={stats?.genreCounts} title="Genre" />
+          </DetailsDiv>
+        </GenStatDetails>
+        <GenStatDetails>
+          <DetailsDiv>
+            <StatByArtist
+              artistStat={stats?.artistAlbumCounts}
+              title="Artist"
+            />
+          </DetailsDiv>
+        </GenStatDetails>
+        <GenStatDetails>
+          <DetailsDiv>
+            <StatByAlbum albumStat={stats?.songStats} title="Album" />
+          </DetailsDiv>
+        </GenStatDetails>
+      </GenStatContainer>
+    </StatContainer>
   );
 };
 
